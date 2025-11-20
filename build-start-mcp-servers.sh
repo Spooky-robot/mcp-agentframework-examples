e8b63089027b15a86e990513ce628290b61b6173
Alexander <3533370+Spooky-robot@users.noreply.github.com>
2025-11-20 13:22:47 +0000

    use buildx instead of build

--- build-start-mcp-servers.sh
+++ build-start-mcp-servers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -f Dockerfile.UserMgmtServer -t usermgmtserver:latest 
+docker buildx build -f Dockerfile.UserMgmtServer -t usermgmtserver:latest 
 docker run -i --rm -p 8080:8080 usermgmtserver:latest --http
